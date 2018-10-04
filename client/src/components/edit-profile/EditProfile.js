f801b4be428fe4c4df4cc409f8e3425f50bf7b46
Your NameTony <tony@wheelertechconsulting.com>
2018-10-04 00:39:37 -0500

    Added education and Experience components

--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import TextFieldGroup from "../common/TextFieldGroup";
 import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
@@ -189,6 +189,9 @@ class CreateProfile extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
               <h1 className="display-4 text-cent">Edit Your Profile</h1>
               <p className="lead text-cente">What's new?</p>
               <small className="d-block pd-">* = required fields</small>
